8e7b1c4a4626c970c5e819d4b905af60b58be67b
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-01-13 15:42:49 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all  code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(3 + 3)
+
+console.log("Adding")
+
+let name = "Ravi" // string
+let age = 19
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);     // object
+console.log(typeof "null");  // string
+console.log(typeof age);  // number
+
+
+
