577bd72f2be494f4a77c9f5991c5810e47867944
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-01-14 13:23:51 +0530

    update operation

--- 01_basics/04_Operation.js
+++ 01_basics/04_Operation.js
@@ -15,3 +15,25 @@ let str2 = " Ravi"
 let str3 = str1 + str2
 console.log(str3);
 
+console.log("1" + 2)      // output 12
+console.log(1 + "2")      // output 12
+console.log("1" + "2")    // output 12
+console.log("1" + 2 + 2)  // output 122
+console.log(1 + 2 + "2")  // output 32
+
+console.log(3 + 4 * 5 % 3);     // output 5
+console.log((3 + 4) * 5 % 3);   // output 2
+
+console.log(true); // output true
+console.log(+true); // output 1  not in use 
+console.log(+""); // output 0    not in use
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
