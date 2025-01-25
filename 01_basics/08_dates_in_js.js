d42e172c43dac7d00f5eb2d4384ea973edf5876b
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-01-25 19:10:26 +0530

    add dates

---
+++ 01_basics/08_dates_in_js.js
@@ -0,0 +1,30 @@
+//***********Dates in JavaScript************
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);  //object
+
+let myCreatedDate = new Date(2025, 1, 26);
+console.log(myCreatedDate.toDateString());
+let myGeneratedDate = new Date("2025-01-26");
+console.log(myGeneratedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// newDate.toLocaleDateString('default',
+//     {
+//         weekday: 'long',
+//     }
+// );
