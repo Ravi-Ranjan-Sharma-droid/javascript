1430376c794a6374800e3af5505cb86d470aa02c
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-02-15 12:09:00 +0000

    update scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,50 @@ if (true) {
     const b = 20
     //var c = 30
     console.log("Inner: ", a);
-    
+
 }
 
 console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one() {
+    const username = "Ravi Ranjan"
+
+    function two() {
+        const website = "github"
+        console.log(username)
+    }
+
+    //console.log(website) error
+    two()
+}
+one()
+
+if (true) {
+    const username = "Snorlax"
+
+    if (username === "Snorlax") {
+        const website = " github"
+        console.log(username + website)
+    }
+
+    //console.log(website); // error
+}
+//console.log(username); // error
+
+// *****************************Intresting**********************************
+
+function addone(num){
+    return num + 1
+}
+console.log(addone(5))
+
+
+//console.log(addtwo(5))
+const addtwo = function(num){
+    return num + 2
+}
+
+
