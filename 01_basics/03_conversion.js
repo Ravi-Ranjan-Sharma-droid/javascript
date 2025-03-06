0cfb0666d6ef1859415aaa8196459a868c696dc8
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-03-06 11:12:21 +0000

    fix 01/03

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+console.log(typeof score);  // string
+console.log(typeof(score)); // string
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);        // NaN
 
 // "33" => 33
 // "33abc" NaN -> not a number
@@ -14,18 +14,19 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn) 
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn); // true
+console.log(typeof booleanIsLoggedIn); // boolean
 
-// 1 => true 
-// 0 => false
-// "" => false 
-// "0" => true
-// "abc" true
+// // 1 => true 
+// // 0 => false
+// // "" => false 
+// // "0" => true
+// // "abc" true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(stringNumber);        // 33
+console.log(typeof stringNumber); // string
 
 
 
