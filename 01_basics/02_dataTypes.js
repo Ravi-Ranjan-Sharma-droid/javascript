c065ea3ebd865fad54db0531cb2918f7fac77010
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-03-06 11:04:07 +0000

    fix 01/02

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,9 +2,9 @@
 
 // alert(3 + 3) // we are using node.js, not browser
 
-console.log(3 + 3)
+console.log(3 + 3) // 6
 
-console.log("Adding")
+console.log("Adding") // Adding
 
 let name = "Ravi" // string
 let age = 19
@@ -12,21 +12,21 @@ let isLoggedIn = false
 let state = null
 
 // number => 2 to power 53
-//bigint 
-//string => ""
-//boolean => true/false
-//null => standalone value
-//undefined =>
-//symbol => unique
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
 
 
-//object
+// //object
 
 
 console.log(typeof undefined); // undefined
-console.log(typeof null);     // object
-console.log(typeof "null");  // string
-console.log(typeof age);    // number
+console.log(typeof null);      // object
+console.log(typeof "null");    // string
+console.log(typeof age);       // number
 
 
 
