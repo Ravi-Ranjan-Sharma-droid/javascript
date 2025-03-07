2433d9f9c77e8982b606472c6426cace42d7d45c
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-03-07 15:19:18 +0000

    fix 01/06

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -3,33 +3,36 @@ const repoCount = 10
 
 // console.log (Name + repoCount + "value") --> old method 
 console.log(`Hello my name is ${Name} and my repo Count is ${repoCount}`);
+          // Hello my nane is Ravi and my repo Count is 10
 
 const gameName = new String("Ravi-Ranjan")
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+console.log(gameName[0]);                 //  R
+console.log(gameName.__proto__);          //  {}
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf("i"));
+console.log(gameName.length);             //  11
+console.log(gameName.toUpperCase());      //  RAVI-RANJAN
+console.log(gameName.charAt(2));          //  v
+console.log(gameName.indexOf("i"));       //  3
 
 const newString = gameName.substring(0, 4)
-console.log(newString);
+console.log(newString);                   //  Ravi
 
-const anotherString = gameName.slice(-9, 4)
+const anotherString = gameName.slice(-9, 4) // vi
 console.log(anotherString);
 
-const newStringOne = "  ravi  "
-console.log(newStringOne);
+const newStringOne = "  ravi  "        //   ravi
+console.log(newStringOne);             // ravi
 console.log(newStringOne.trim());
+
+//***********************************************************************
+
 const url = "https//google.com"
-console.log(url);
-console.log(url.replace("google", "github"));
-console.log(url.includes("google"));
-console.log(url.includes("github"));
+console.log(url);    //   https//google.com
+console.log(url.replace("google", "github"));    // https//github.com
+console.log(url.includes("google"));   //  true
+console.log(url.includes("github"));   // false
 
-console.log(gameName.split("-"));
-
+console.log(gameName.split("-"));      // [ 'Ravi', 'Ranjan']
 
 
