b7832006c66d4e68b2c27c30289312b2587bb53a
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-03-15 04:28:45 +0530

    useless commit 05/06 file created noting else

---
+++ 05_iterations/06_map.js
@@ -0,0 +1,11 @@
+// const coding = ["js", "ruby", "python", "cpp", "rb"];
+
+
+// const values = coding.forEach((man) => {
+//     // console.log(man);
+//     return man
+// });
+
+// console.log(values);
+
+
