88d21fede417ae7f989a47b1d62bffd0c62c9fb4
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-04-29 00:13:37 +0530

    remove comment

--- 10_Advance-topic/promices.js
+++ 10_Advance-topic/promices.js
@@ -1,5 +1,3 @@
-/*
-
 const promiseOne = new Promise(function (resolve, reject) {
          // async task , db call, cryptography, network
     setTimeout(function () {
@@ -99,7 +97,7 @@ const promiseOne = new Promise(function (resolve, reject) {
     }
   }
   consumePromiseFive();
-  */
+  
 async function getAllUsers() {
   try {
       const response = await fetch("https://jsonplaceholder.typicode.com/users");
